75b0e4f4984038059fb5ceab8d1aec394be26c34
Yohann D.E <yohann.degli-esposti@epita.fr>
2022-12-01 19:45:25 +0100

    day1: pushing code.

--- AdventOfCode2022/Day1/day1.go
+++ AdventOfCode2022/Day1/day1.go
@@ -1,9 +1,55 @@
 package main
 
 import (
-	"fmt"
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Hello, Day1")
+	fmt.Println(Challenge("./chall1_input.txt"));
+}
+
+func Challenge(fileName string) (int, int) {
+	readfile, err := os.Open(fileName);
+
+	if err != nil {
+		fmt.Println(err);
+		return -1, -1;
+	}
+
+	fileScanner := bufio.NewScanner(readfile);
+	fileScanner.Split(bufio.ScanLines);
+
+	max := -1;
+	max3 := []int{0,0,0}
+	calories_accumulated := 0;
+
+	for fileScanner.Scan() {
+		str := fileScanner.Text();
+		calories, err := strconv.Atoi(str);
+		if err == nil {
+			calories_accumulated += calories;
+		} else {
+			if calories_accumulated > max {
+				max = calories_accumulated;
+			}
+			if calories_accumulated > max3[0]{
+				max3[0] = calories_accumulated;
+				sort.Ints(max3);
+			}
+			calories_accumulated = 0;
+		}
+	}
+
+	max3Cumulated := 0;
+
+	for _, v := range max3{
+		max3Cumulated += v;
+	}
+
+	readfile.Close();
+	return max, max3Cumulated;
 }
